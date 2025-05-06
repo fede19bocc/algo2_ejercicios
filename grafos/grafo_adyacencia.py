77a1c316789024e746cf9723a1ebf4de2ef13fc7
Fede <fedeboccazzi@gmail.com>
2025-05-06 16:38:23 -0300

    agregue Valuerrors y validaciones en agregar_nodo y agregar_Arista

--- grafos/grafo_adyacencia.py
+++ grafos/grafo_adyacencia.py
@@ -13,21 +13,31 @@ class GrafoAdy():
         return nodo in self.lista_adyacencia.keys()
     
     def agregar_nodo(self, nodo: T):
-        self.lista_adyacencia[nodo] = set()
+        if not self.es_nodo(nodo):
+            self.lista_adyacencia[nodo] = set()
         
     def agregar_arista(self, origen: T, destino: T):
         if origen in self.lista_adyacencia.keys():
             self.lista_adyacencia[origen].add(destino)
+        else:
+            raise ValueError(f'{origen} no es un nodo del grafo')
             
     def eliminar_arista(self, origen: T, destino: T):
-        if origen in self.lista_adyacencia.keys():
+        if (self.es_nodo(origen)) and (destino in self.lista_adyacencia[origen]):
             self.lista_adyacencia[origen].remove(destino)
+        elif (self.es_nodo(destino)) and (origen in self.lista_adyacencia[destino]):
+            self.lista_adyacencia[destino].remove(origen)
+        else:
+            raise ValueError(f'La arista ({origen},{destino}) no existe en el grafo')
             
     def eliminar_nodo(self, nodo: T):
-        del self.lista_adyacencia[nodo]
-        for llave in self.lista_adyacencia.keys():
-            if nodo in self.lista_adyacencia[llave]:
-                self.lista_adyacencia[llave].remove(nodo)
+        if self.es_nodo(nodo):
+            del self.lista_adyacencia[nodo]
+            for llave in self.lista_adyacencia.keys():
+                if nodo in self.lista_adyacencia[llave]:
+                    self.lista_adyacencia[llave].remove(nodo)
+        else:
+            raise ValueError(f'El nodo {nodo} no pertenece al grafo')
         
     def es_vecino_de(self, nodo: T, otro_nodo: T) -> bool:
         if nodo in self.lista_adyacencia.keys():
@@ -51,7 +61,10 @@ def main():
     print(migrafo.lista_adyacencia)
     print("Es vecino 2 de 3",migrafo.es_vecino_de(2,3))
     print("Es vecino 4 de 1",migrafo.es_vecino_de(4,1))
-    migrafo.eliminar_nodo(3)
+    # print("elimino arista (4,2)")
+    # migrafo.eliminar_arista(4,2)
+    print("elimino nodo 5")
+    migrafo.eliminar_nodo(5)
     print(migrafo.lista_adyacencia)
     print(migrafo.nodos())
     print("es nodo 5: ", migrafo.es_nodo(5))
